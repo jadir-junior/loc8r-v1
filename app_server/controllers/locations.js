5ac06dc2e7cd656e3b6cd028e08a877a75903d1c
jadir-junior <jadirjsjunior@gmail.com>
2016-12-15 21:06:31 -0200

    Ensuring forms work as expected

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -73,7 +73,8 @@ const renderReviewForm = (req, res, locDetail) => {
   res.render('location-review-form', {
     title: 'Review ' + locDetail.name + ' on Loc8r',
     pageHeader: { title: 'Review ' + locDetail.name },
-    error: req.query.err
+    error: req.query.err,
+    url: req.originalUrl
   });
 }
 
