faafe4e3b4514746dc8c319e57d066bb11213a06
jadir-junior <jadirjsjunior@gmail.com>
2016-12-16 14:25:41 -0200

    Adding the first views, controllers and services

--- app_client/app.js
+++ app_client/app.js
@@ -3,7 +3,9 @@ angular.module('loc8rApp', ['ngRoute']);
 function config ($routeProvider) {
   $routeProvider
     .when('/', {
-
+      templateUrl: 'home/home.view.html',
+      controller: 'homeCtrl',
+      controllerAs: 'vm'
     })
     .otherwise({ redirectTo: '/' });
 }
