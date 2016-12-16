214df95782e24dbd9ff9e319f4e0b380a9cd5528
jadir-junior <jadirjsjunior@gmail.com>
2016-12-16 17:01:58 -0200

    A full SPA removing reliance ont he server-side application

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ require('./app_api/models/db');
 var uglifyJs = require('uglify-js');
 var fs = require('fs');
 
-var routes = require('./app_server/routes/index');
+// var routes = require('./app_server/routes/index');
 const routesApi = require('./app_api/routes/locations');
-var users = require('./app_server/routes/users');
+// var users = require('./app_server/routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -23,7 +24,10 @@ var appClientFiles = [
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
   'app_client/common/filters/formatDistance.filter.js',
-  'app_client/common/directives/ratingStars/ratingStars.directive.js'
+  'app_client/common/directives/ratingStars/ratingStars.directive.js',
+  'app_client/common/directives/footerGeneric/footerGeneric.directive.js',
+  'app_client/common/directives/navigation/navigation.directive.js',
+  'app_client/common/directives/pageHeader/pageHeader.directive.js'
 ];
 var uglified = uglifyJs.minify(appClientFiles, { compress: false });
 
@@ -50,9 +54,13 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/api', routesApi);
-app.use('/users', users);
+// app.use('/users', users);
+
+app.use(function(req, res){
+  res.sendfile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app_client/common/directives/navigation/navigation.directive.js
@@ -0,0 +1,10 @@
+(function() {
+  angular.module('loc8rApp').directive('navigation', navigation);
+
+  function navigation() {
+    return {
+      restric: 'AE',
+      templateUrl: '/common/directives/navigation/navigation.template.html'
+    }
+  }
+})();
