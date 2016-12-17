5bd631043c3b6a3aeef0f349806ee331bc4c3800
jadir-junior <jadirjsjunior@gmail.com>
2016-12-17 16:43:44 -0200

    Creating Angular authentication service

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ var appClientFiles = [
   'app_client/locationDetail/locationDetail.controller.js',
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
+  'app_client/common/services/authentication.service.js',
   'app_client/common/filters/formatDistance.filter.js',
   'app_client/common/filters/addHtmlLineBreaks.filter.js',
   'app_client/common/directives/ratingStars/ratingStars.directive.js',
